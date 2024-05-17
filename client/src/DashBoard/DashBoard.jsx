bf215a7a6291d70d7dd2313b4660078d1a867048
TusharSukhanandi <156135354+TusharSukhanandi@users.noreply.github.com>
2024-05-17 12:29:47 +0530

    Update DashBoard.jsx

--- client/src/DashBoard/DashBoard.jsx
+++ client/src/DashBoard/DashBoard.jsx
@@ -88,7 +88,10 @@ function Dashboard() {
 
       axios
         .post(`${URL}/update/${id}`, updatedata)
-        .then((res) => setData(res.data))
+        .then((res) => {
+          setData(res.data)
+          console.log(res.data)
+        })
         .catch((err) => console.log(err));
 
       location.reload();
