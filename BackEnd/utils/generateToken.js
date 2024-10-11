033f625916fff3d30822da4e4c051153fc54dad6
Tushar Sukhananadi <tusharsukhanandi@yahoo.com>
2024-10-11 19:29:44 +0530

    solving cookie problem

--- BackEnd/utils/generateToken.js
+++ BackEnd/utils/generateToken.js
@@ -6,6 +6,7 @@ const generateTokenAndSetToken = (userId, res) => {
    return res.cookie("TrackItJwt", token, {
         maxAge : 15 * 24 * 60 * 60 * 1000,
         httpOnly : true,
+        secure : true,
     })
 }
 
